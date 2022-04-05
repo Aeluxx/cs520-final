3655f3e9401210a81fe7cca34914eb2bea4d499c
Ric Donati <rdonati5645@gmail.com>
2022-04-05 16:47:01 -0400

    Create index for backend, dont track node modules

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+// import path from 'path'
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+// app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Test endpoint
+app.get('/', (request, response) => {
+  response.json({ message: `Welcome to backend!!` })
+})
+
+// Example of using different routers
+// app.use('/model1', model1Router)
+// app.use('/model2', model2Router)
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`)
+})
