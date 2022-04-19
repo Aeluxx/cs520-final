1c7131aff35cedb295a00c088d1cc018d899b634
Ric Donati <rdonati5645@gmail.com>
2022-04-19 16:52:39 -0400

    Adds login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 import Dashboard from './pages/Dashboard'
 import Edit from './pages/Edit'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import Login from './pages/Login'
 declare module '@mui/material/styles' {
   interface Theme {
     background: {
@@ -36,6 +37,7 @@ const App = () => {
         <NavBar />
         <Routes>
           <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
           <Route path='edit' element={<Edit />}>
             <Route path=':docId' element={<Edit />} />
           </Route>
