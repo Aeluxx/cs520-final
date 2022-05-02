af0f356c774af9d86d235a207e1a0abbd912cf90
MiniShark27 <kobifalus@gmail.com>
2022-05-02 17:00:09 -0400

    fix bugs

--- pages/api/FavoriteNotes.ts
+++ pages/api/FavoriteNotes.ts
@@ -3,6 +3,7 @@ import connectDB, { disconnectDb } from "../../middleware/connectDb";
 import Notes from "../../models/note";
 import nc from "next-connect";
 import User from "../../models/user";
+import { ObjectId } from "mongodb";
 
 const handler = nc();
 
@@ -21,9 +22,9 @@ handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
 // Requires Body: { userId: string, noteId: string }
 handler.delete(async (req: NextApiRequest, res: NextApiResponse) => {
   await connectDB();
-  const user = await User.findOne({ _id: req.query.userId });
+  const user = await User.findOne({ _id: req.body.userId });
   user.favoriteNoteIds = user.favoriteNoteIds.filter(
-    (id: any) => id.toString() !== req.query.noteId
+    (id: any) => id.toString() !== req.body.noteId
   );
   await user.save();
   res.status(200).json(user);
@@ -34,7 +35,12 @@ handler.delete(async (req: NextApiRequest, res: NextApiResponse) => {
 handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
   await connectDB();
   const user = await User.findOne({ _id: req.body.userId });
-  user.favoriteNoteIds = user.favoriteNoteIds.push(req.body.noteId);
+  // const x = ObjectId(req.body.noteId);
+  // console.log(x);
+  if (!user.favoriteNoteIds.includes(req.body.noteId)) {
+    user.favoriteNoteIds.push(req.body.noteId);
+  }
+  // user.favoriteNoteIds = user.favoriteNoteIds.concat(req.body.noteId);
   await user.save();
   res.status(200).json(user);
   disconnectDb();
