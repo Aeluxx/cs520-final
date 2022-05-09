c800d49de81e2cc600e1bfe66332ae4decb3aa36
Aeluxx <matthewgregory@me.com>
2022-05-08 20:48:54 -0400

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const nextJest = require("next/jest");
+//const createJestConfig = nextJest({
+//    dir: "./",
+//});
+//const customJestConfig = {
+//    moduleDirectories: ["node_modules", "<rootDir>/"],
+//    testEnvironment: "jest-environment-jsdom",
+//};
+//module.exports = createJestConfig(customJestConfig);
+
+module.exports = {
+    moduleDirectories: ["node_modules", "<rootDir>/"],
+    testEnvironment: 'jest-environment-jsdom',
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: {
+                jsx: 'react-jsx',
+            },
+        },
+    },
+}
