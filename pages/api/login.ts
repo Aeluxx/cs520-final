591276eea75f3f148ab7b4394697c7accf97f0cd
Aeluxx <matthewgregory@me.com>
2022-05-09 23:50:02 -0400

    Add files via upload

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,7 +3,6 @@ import { NextApiRequest } from 'next'
 import UserModel from '../../models/user'
 import connectDB from '../../middleware/connectDb'
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import nc from 'next-connect'
 
 const handler = nc()
@@ -12,13 +11,9 @@ handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     const { email, password } = req.body
-    const user = await UserModel.findOne({ email })
-
+    const user = await UserModel.findOne({ email, password })
     if (!user) {
       res.status(500).send(`No user with email ${email}`)
-    }
-    else if (!bcrypt.compareSync(password, user.password)){
-      res.status(500).send('Invalid password')
     }
     const token = jwt.sign({ name: user.name, email: user.email, id: user._id }, process.env.JWT_SECRET as string)
     res.status(200).send({ user, token })
